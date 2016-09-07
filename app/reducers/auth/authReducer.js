987c83a95cf1574c31aaec1485f4e84e2a349319
touna <1054404065@qq.com>
2016-09-07 18:36:52 +0800

    update

--- app/reducers/auth/authReducer.js
+++ app/reducers/auth/authReducer.js
@@ -9,24 +9,26 @@ export default function authReducer(state = initialState, action) {
 	}
 
 	switch (action.type) {
-		case ActionTypes.FETCH_LOGIN_START: 														// 登录请求开始
-			return state.setIn(['login', 'isFetching'], true);
-		case ActionTypes.FETCH_LOGIN_SUCCESS: 													// 登录成功
-			return state.setIn(['login', 'isFetching'], false)
-									.setIn(['login', 'isLoginSuccess'], true);
-		case ActionTypes.FETCH_LOGIN_FAILD: 														// 登录失败
-			return state.setIn(['login', 'isFetching'], false)
-									.setIn(['login', 'isLoginSuccess'], false);
-		case ActionTypes.FETCH_REGISTER_START: 														// 注册请求开始
-			return state.setIn(['register', 'isFetching'], true);
-		case ActionTypes.FETCH_REGISTER_SUCCESS: 													// 注册成功
-			return state.setIn(['register', 'isFetching'], false)
-									.setIn(['register', 'isRegisterSuccess'], true);
-		case ActionTypes.FETCH_REGISTER_FAILD: 														// 注册失败
-			return state.setIn(['register', 'isFetching'], false)
-									.setIn(['register', 'isRegisterSuccess'], false);
-		// no default
+	case ActionTypes.FETCH_LOGIN_START: 														// 登录请求开始
+		return state.setIn(['login', 'isFetching'], true);
+	case ActionTypes.FETCH_LOGIN_SUCCESS: 													// 登录成功
+		return state.setIn(['login', 'isFetching'], false)
+								.setIn(['login', 'isLoginSuccess'], true);
+	case ActionTypes.FETCH_LOGIN_FAILD: 														// 登录失败
+		return state.setIn(['login', 'isFetching'], false)
+								.setIn(['login', 'isLoginSuccess'], false);
+	case ActionTypes.FETCH_REGISTER_START: 														// 注册请求开始
+		return state.setIn(['register', 'isFetching'], true);
+	case ActionTypes.FETCH_REGISTER_SUCCESS: 													// 注册成功
+		return state.setIn(['register', 'isFetching'], false)
+								.setIn(['register', 'isRegisterSuccess'], true);
+	case ActionTypes.FETCH_REGISTER_FAILD: 														// 注册失败
+		return state.setIn(['register', 'isFetching'], false)
+								.setIn(['register', 'isRegisterSuccess'], false);
+	// no default
+	default:
+		return state;
 	}
 
-	return state;
+	// return state;
 }
